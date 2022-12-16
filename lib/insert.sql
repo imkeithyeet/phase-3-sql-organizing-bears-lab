420be38adc3824f6e01846b11a33d2fd8224a97f
Keith Ma <keithchezzy@gmail.com>
2022-12-15 17:24:22 -0800

    commit tree 4ba42a1a397da177b27b9f3db07b02003b898829

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES ('Mr.Chocolate', 2, "M", "chocolate", "asshole", "1");
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES ('Rowdy', 2, "F", "chocolate", "asshole", "1");
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES ('Tabitha', 2, "F", "chocolate", "asshole", "1");
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES ('Sergeant Brown', 2, "F", "chocolate", "asshole", "1");
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES ('Melissa', 2, "F", "chocolate", "asshole", "1");
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES ('Grinch', 2, "F", "chocolate", "asshole", "1");
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES ('Wendy', 2, "M", "chocolate", "asshole", "1");
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES (null, null, null, null, null, null);
+
